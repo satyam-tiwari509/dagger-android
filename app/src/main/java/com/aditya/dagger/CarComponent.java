d9cac3270519e78c2e1ede229537c64d81065a9a
Aditya <icgadi99@gmail.com>
2020-11-16 16:20:45 +0530

    Gen- Component classes and impl Constructor Injection

---
+++ app/src/main/java/com/aditya/dagger/CarComponent.java
@@ -0,0 +1,8 @@
+package com.aditya.dagger;
+
+import dagger.Component;
+
+@Component
+public interface CarComponent {
+    Car getCar();
+}
--- app/src/main/java/com/aditya/dagger/MainActivity.java
+++ app/src/main/java/com/aditya/dagger/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import dagger.internal.DaggerCollections;
+
 public class MainActivity extends AppCompatActivity {
+    private Car car;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CarComponent component = DaggerCarComponent.create();
+        car = component.getCar();
+        car.drive();
     }
 }
